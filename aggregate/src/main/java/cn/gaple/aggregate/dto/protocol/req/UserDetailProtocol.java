450dc6254ae8f1e152015c838c697a49121a3fc9
britton <britton@126.com>
2021-10-27 17:34:14 +0800

    * 重构

--- aggregate/src/main/java/cn/gaple/aggregate/dto/protocol/req/UserDetailProtocol.java
+++ aggregate/src/main/java/cn/gaple/aggregate/dto/protocol/req/UserDetailProtocol.java
@@ -13,4 +13,8 @@ public class UserDetailProtocol extends GXBaseReqProtocol {
     @NotEmpty(message = "地址不能为空")
     @NotNull(message = "地址不能为空")
     private String address;
+
+    @NotEmpty(message = "省不能为空")
+    @NotNull(message = "省不能为空")
+    private String province;
 }
