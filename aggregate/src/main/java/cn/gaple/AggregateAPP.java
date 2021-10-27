9f5cf4966353f2fafeaa9b53220e9d807dc1a677
britton <britton@126.com>
2021-10-27 18:42:21 +0800

    * 重构

--- aggregate/src/main/java/cn/gaple/AggregateAPP.java
+++ aggregate/src/main/java/cn/gaple/AggregateAPP.java
@@ -3,9 +3,11 @@ package cn.gaple;
 import com.geoxus.core.framework.annotation.GXEnableLeafFramework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @GXEnableLeafFramework
+@EnableAspectJAutoProxy
 public class AggregateAPP {
     public static void main(String[] args) {
         SpringApplication.run(AggregateAPP.class, args);
--- aggregate/src/main/java/cn/gaple/GXCoreApplication.java
+++ aggregate/src/main/java/cn/gaple/GXCoreApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@SpringBootApplication
-@EnableCaching
-@EnableAspectJAutoProxy
+//@SpringBootApplication
+//@EnableCaching
+//@EnableAspectJAutoProxy
 public class GXCoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(GXCoreApplication.class, args);
