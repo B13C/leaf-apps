999d33a043a0e2026782f3ac83c900532286777f
britton <britton@126.com>
2021-10-28 17:05:14 +0800

    + 重构

--- user/user-server/src/main/java/cn/gaple/user/controller/IndexController.java
+++ user/user-server/src/main/java/cn/gaple/user/controller/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/index/frontend1/")
 public class IndexController {
--- user/user-web-api/src/main/java/cn/gaple/UserApplication.java
+++ user/user-web-api/src/main/java/cn/gaple/UserApplication.java
@@ -1,11 +1,13 @@
 package cn.gaple;
 
+import com.geoxus.core.framework.annotation.GXEnableLeafFramework;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableDubbo
+@GXEnableLeafFramework
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
