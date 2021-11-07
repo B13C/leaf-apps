028f806b80e7f5852178ae71a28efefeb431a770
britton <britton@126.com>
2021-11-07 15:12:34 +0800

    *fix 新增组件

--- rbac/rbac-server/src/main/java/cn/gaple/rbac/presenter/GXAdminPresenter.java
+++ rbac/rbac-server/src/main/java/cn/gaple/rbac/presenter/GXAdminPresenter.java
@@ -1,8 +1,11 @@
 package cn.gaple.rbac.presenter;
 
-import cn.maple.core.framework.presenter.GXBasePresenter;
+import cn.maple.core.framework.ddd.presenter.GXBasePresenter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GXAdminPresenter implements GXBasePresenter {
+    public String fullName() {
+        return "Hello World!!!";
+    }
 }
--- rbac/rbac-server/src/main/java/cn/gaple/rbac/repository/GXAdminRepository.java
+++ rbac/rbac-server/src/main/java/cn/gaple/rbac/repository/GXAdminRepository.java
@@ -1,26 +1,67 @@
 package cn.gaple.rbac.repository;
 
-import cn.gaple.rbac.core.constant.GXAdminConstant;
 import cn.gaple.rbac.dao.GXAdminDao;
+import cn.gaple.rbac.dto.req.GXAdminReqDto;
 import cn.gaple.rbac.dto.res.GXAdminResDto;
-import cn.hutool.core.collection.CollUtil;
-import cn.maple.core.framework.repository.GXBaseRepository;
+import cn.gaple.rbac.mapstruct.req.GXAdminReqMapStruct;
+import cn.gaple.rbac.presenter.GXAdminPresenter;
+import cn.maple.core.framework.ddd.repository.GXBaseRepository;
+import cn.maple.core.framework.dto.inner.res.GXPaginationResDto;
 import com.google.common.collect.Table;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.Resource;
 import java.util.List;
 import java.util.Set;
 
 @Repository
-public class GXAdminRepository implements GXBaseRepository {
-    /**
-     * 管理员DAO
-     */
+public class GXAdminRepository implements GXBaseRepository<GXAdminReqDto, GXAdminResDto> {
+    @Resource
     private GXAdminDao adminDao;
 
+    @Resource
+    private GXAdminReqMapStruct adminReqMapStruct;
+
+    @Resource
+    private GXAdminPresenter adminPresenter;
+
     @Override
-    public <R> List<R> all(Set<String> set, Table<String, String, Object> condition) {
-        List<GXAdminResDto> list = adminDao.getListByCondition(GXAdminConstant.TABLE_NAME, CollUtil.newHashSet("*"), condition);
+    public Integer create(GXAdminReqDto gxAdminReqDto) {
         return null;
+    }
+
+    @Override
+    public Integer update(GXAdminReqDto gxAdminReqDto) {
+        return null;
+    }
+
+    @Override
+    public Integer updateOrCreate(GXAdminReqDto gxAdminReqDto) {
+        return null;
+    }
+
+    @Override
+    public List<GXAdminResDto> all(Set<String> set, Table<String, String, Object> table) {
+        return null;
+    }
+
+    @Override
+    public GXAdminResDto first(Set<String> set, Table<String, String, Object> table) {
+        return null;
+    }
+
+    @Override
+    public GXAdminResDto findByCondition(Table<String, String, Object> table) {
+        return null;
+    }
+
+    @Override
+    public GXPaginationResDto<GXAdminResDto> paginate(Integer integer, Set<String> set) {
+        return null;
+    }
+
+    @Override
+    public int deleteWhere(Table<String, String, Object> table) {
+        return 0;
     }
 }
