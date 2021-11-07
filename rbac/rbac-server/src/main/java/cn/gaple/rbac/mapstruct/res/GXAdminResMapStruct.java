bdf17ef402256130570609f989719d6dca6f6500
britton <britton@126.com>
2021-11-07 15:36:07 +0800

    *fix 新增组件

---
+++ rbac/rbac-server/src/main/java/cn/gaple/rbac/mapstruct/res/GXAdminResMapStruct.java
@@ -0,0 +1,11 @@
+package cn.gaple.rbac.mapstruct.res;
+
+import cn.gaple.rbac.dto.res.GXAdminResDto;
+import cn.gaple.rbac.entities.GXAdminEntity;
+import cn.maple.core.framework.mapstruct.GXBaseMapStruct;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface GXAdminResMapStruct extends GXBaseMapStruct<GXAdminEntity, GXAdminResDto> {
+}
--- rbac/rbac-server/src/main/java/cn/gaple/rbac/repository/GXAdminRepository.java
+++ rbac/rbac-server/src/main/java/cn/gaple/rbac/repository/GXAdminRepository.java
@@ -4,6 +4,7 @@ import cn.gaple.rbac.dao.GXAdminDao;
 import cn.gaple.rbac.dto.req.GXAdminReqDto;
 import cn.gaple.rbac.dto.res.GXAdminResDto;
 import cn.gaple.rbac.mapstruct.req.GXAdminReqMapStruct;
+import cn.gaple.rbac.mapstruct.res.GXAdminResMapStruct;
 import cn.gaple.rbac.presenter.GXAdminPresenter;
 import cn.maple.core.framework.ddd.repository.GXBaseRepository;
 import cn.maple.core.framework.dto.inner.res.GXPaginationResDto;
@@ -21,6 +22,9 @@ public class GXAdminRepository implements GXBaseRepository<GXAdminReqDto, GXAdmi
 
     @Resource
     private GXAdminReqMapStruct adminReqMapStruct;
+
+    @Resource
+    private GXAdminResMapStruct adminResMapStruct;
 
     @Resource
     private GXAdminPresenter adminPresenter;
