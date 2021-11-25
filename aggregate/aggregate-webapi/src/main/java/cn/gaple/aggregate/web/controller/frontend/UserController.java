e2f9d8d17b11adca0f381274e56177bfb8af734b
britton <britton@126.com>
2021-11-25 16:33:21 +0800

    *fix BUG Fixed

--- aggregate/aggregate-webapi/src/main/java/cn/gaple/aggregate/web/controller/frontend/UserController.java
+++ aggregate/aggregate-webapi/src/main/java/cn/gaple/aggregate/web/controller/frontend/UserController.java
@@ -1,7 +1,9 @@
 package cn.gaple.aggregate.web.controller.frontend;
 
 import cn.gaple.aggregate.web.dto.protocol.req.UserReqProtocol;
+import cn.gaple.properties.LightTypeProperties;
 import cn.gaple.rbac.api.HelloApi;
+import cn.hutool.core.lang.Dict;
 import cn.maple.core.framework.annotation.GXRequestBody;
 import cn.maple.core.framework.util.GXResultUtils;
 import org.apache.dubbo.config.annotation.DubboReference;
@@ -9,16 +11,23 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
 public class UserController {
     @DubboReference
     private HelloApi helloApi;
+
+    @Resource
+    private LightTypeProperties lightTypeProperties;
 
     @PostMapping("index")
     public GXResultUtils<String> hello(@GXRequestBody @Validated UserReqProtocol userReqProtocol) {
         System.out.println(userReqProtocol);
         System.out.println(userReqProtocol.getAuthor());
         String s = "";
+        List<Dict> types = lightTypeProperties.getTypes();
         return GXResultUtils.ok("Hello World" + helloApi.hello("AAAABBBB"));
     }
 }
