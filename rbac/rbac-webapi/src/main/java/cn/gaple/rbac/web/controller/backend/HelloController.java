6bdeceb5e520f9077e3b61bb7fb7caaad3d30581
britton <britton@126.com>
2021-12-16 15:05:34 +0800

    *fix 框架重构升级

--- rbac/rbac-webapi/src/main/java/cn/gaple/rbac/web/controller/backend/HelloController.java
+++ rbac/rbac-webapi/src/main/java/cn/gaple/rbac/web/controller/backend/HelloController.java
@@ -3,19 +3,14 @@ package cn.gaple.rbac.web.controller.backend;
 import cn.gaple.rbac.mapper.GXAdminMapper;
 import cn.gaple.rbac.repository.GXAdminRepository;
 import cn.gaple.rbac.web.dto.protocol.TestProtocol;
-import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.lang.Dict;
-import cn.maple.core.datasource.constant.GXBuilderConstant;
 import cn.maple.core.framework.annotation.GXRequestBody;
 import cn.maple.core.framework.controller.GXBaseController;
 import cn.maple.core.framework.util.GXResultUtils;
-import com.google.common.collect.HashBasedTable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 
 /**
@@ -32,13 +27,7 @@ public class HelloController implements GXBaseController {
 
     @PostMapping("test")
     public GXResultUtils<String> test(@GXRequestBody TestProtocol req) {
-        HashBasedTable<String, String, Object> condition = HashBasedTable.create();
-        condition.put("id", GXBuilderConstant.NUMBER_EQ, 1);
-        condition.put(GXBuilderConstant.DELETED_FLAG_FIELD_NAME, GXBuilderConstant.EXCLUSION_DELETED_CONDITION_FLAG, "");
-        HashBasedTable<String, String, Object> condition1 = HashBasedTable.create();
-        condition1.put("id", GXBuilderConstant.NUMBER_EQ, 1);
-        List<Dict> data = adminRepository.findByCondition("s_admin", condition1, CollUtil.newHashSet("nickname", "username"), Dict.class);
-        List<String> nickname = adminRepository.findByCondition("s_admin", condition, CollUtil.newHashSet("nickname"), String.class);
-        return GXResultUtils.ok("Hello : " + nickname);
+        String[] names = {""};
+        return GXResultUtils.ok("Hello : " + "World");
     }
 }
